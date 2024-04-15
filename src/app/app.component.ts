6a17ae685dd05bedd066ef7a0c5d6b4b67d49df6
sivakumar6678 <chandragarisivakumar@gmail.com>
2024-04-15 17:02:03 +0530

    Refactor code to improve performance and readability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { RegistrationFormComponent } from './pages/registration-form/registration-form.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LoginSignupButtonsComponent } from "./pages/login-signup-buttons/login-signup-buttons.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, DashboardComponent, RegistrationFormComponent,LoginComponent]
+    imports: [RouterOutlet, DashboardComponent, RegistrationFormComponent, LoginComponent, LoginSignupButtonsComponent]
 })
 export class AppComponent {
   title = 'EduHub';
